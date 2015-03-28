0bf0cf521de575b30d591285fde1daab6c2b2e8a
anyellezanatta <anyelle.ad@gmail.com>
2015-03-28 00:59:27 -0300

    adicionei login

---
+++ Bonificacao.Web/Controllers/ContaController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Bonificacao.Web.Controllers
+{
+    [AllowAnonymous]
+    public class ContaController : Controller
+    {
+        // GET: Conta
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+
+        // POST: Conta/Login
+        [HttpPost]
+        public ActionResult Login(string usuario, string senha, bool? lembrar)
+        {
+            try
+            {              
+                FormsAuthentication.SetAuthCookie(usuario, false);
+                return RedirectToAction("Index", "Home");       
+        
+            }
+            catch
+            {
+                return View();
+            }
+        }        
+    }
+}
