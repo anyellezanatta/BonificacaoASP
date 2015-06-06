8abff3ad57799785658763c51ffd5107a046117a
anyelle.ad@gmail.com <anyelle.ad@gmail.com>
2015-06-05 23:08:41 -0300

    delete ajax; terminar box

--- Bonificacao.Web/HtmlHelpers/PanelHelpers.cs
+++ Bonificacao.Web/HtmlHelpers/PanelHelpers.cs
@@ -4,33 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace Bonificacao.Web.HtmlHelpers
 {
     public static class PanelHelpers
     {
-        public static IHtmlString Box(this HtmlHelper htmlHelper,string title, string description, Func<object, object> panelMarkup = null)
+        public static void Box(this HtmlHelper htmlHelper,string title, string description, Func<object, object> panelMarkup = null)
         {
-            return Box(htmlHelper, title, description, (panelMarkup == null ? "" : panelMarkup.DynamicInvoke(htmlHelper.ViewContext).ToString()));
+            Box(htmlHelper, title, description, (panelMarkup == null ? "" : panelMarkup.DynamicInvoke(htmlHelper.ViewContext).ToString()));
         }
 
-        public static IHtmlString Box(this HtmlHelper html, string title, string description, string content)
+        public static void Box(this HtmlHelper html, string title, string description, string content)
         {
-            string result =
-                "<div class=\"row\">" +
-                    "<div class=\"col-lg-12\">" +
-                        "<div class=\"ibox\">" +
-                            "<div class=\"ibox-content\">" +
-                                "<h2>" + title + "</h2>" +
-                                (!string.IsNullOrEmpty(description) ? "<small>" + description + "</small>" : string.Empty) +
-                                "<div class=\"padding-top\">" +
-                                content +
-                                "</div>" +
-                            "</div>" +
-                        "</div>" +
-                    "</div>" +
-                "</div>";
-            return new HtmlString(result.ToString());
+            using (var writer = new HtmlTextWriter(html.ViewContext.Writer))
+            {
+                //string result =
+                writer.WriteLine("<div class=\"row\">");
+                writer.WriteLine("<div class=\"col-lg-12\">");
+                writer.WriteLine("<div class=\"ibox\">");
+                writer.WriteLine("<div class=\"ibox-content\">");
+                writer.WriteLine("<h2>" + title + "</h2>");
+                writer.WriteLine((!string.IsNullOrEmpty(description) ? "<small>" + description + "</small>" : string.Empty));
+                writer.WriteLine("<div class=\"padding-top\">");
+                writer.WriteLine(content);
+                writer.WriteLine("</div>");
+                writer.WriteLine("</div>");
+                writer.WriteLine("</div>");
+                writer.WriteLine("</div>");
+                writer.WriteLine("</div>");
+            }
+            //return new HtmlString(result.ToString());
         }
+
+        //public void Render(this HtmlHelper htmlHelper)
+        //{
+        //    using (var writer = new HtmlTextWriter(htmlHelper.ViewContext.Writer))
+        //        WriteHtml(writer);
+        //}
     }
 }
